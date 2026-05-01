1110443725cb3816121989563f6669d4b310ea3a
Erast <erast.nakhod@bk.ru>
2026-05-02 00:36:25 +0300

    Конфиг деплоя

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      /\.vercel\.app$/,
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
-  });
+  });;
 
   await app.listen(process.env.PORT ?? 3000);
 }
