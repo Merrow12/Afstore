2e966d2cf7c2a0e353f8b96ee15cc9453b63f843
Erast <erast.nakhod@bk.ru>
2026-05-02 00:49:55 +0300

    fix CORS vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,11 +8,14 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'http://127.0.0.1:5173',
+      'https://afstore.vercel.app',
+      'https://afstore-git-main-merrow-s-projects.vercel.app',
+      'https://afstore-la2e9p8cl-merrow-s-projects.vercel.app',
       /\.vercel\.app$/,
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
-  });;
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
